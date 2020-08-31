0016a82fa7b33888d467d3cfb3848481b4934b35
sabatex <serhiy.lakas@hotmail.com>
2020-08-31 11:15:41 +0300

    first commit

---
+++ src/ComponentModel/DataAnnotations/Description1C8.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+namespace sabatex.V1C8.ComponentModel.DataAnnotations
+{
+    /// <summary>
+    ///     DisplayEnumAttribute is a general-purpose attribute to specify user-visible globalizable strings for enum.
+    ///     The string properties of this class can be used either as literals or as resource identifiers into a specified
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Enum | AttributeTargets.Field ,AllowMultiple=false) ]
+    public sealed class Description1C8Attribute:Attribute
+    {
+        public string Name { get; set; }  
+        public string Comment { get; set; } 
+        public string Synonym { get; set; }  
+
+    }
+ 
+
+}
---
+++ src/EnumExtensions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sabatex.V1C8
+{
+    public static class EnumExtensions 
+    {
+
+    }
+}

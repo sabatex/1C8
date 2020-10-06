05be403e321c5505a5ef0811644cd988b132dfb5
sabatex <serhiy.lakas@hotmail.com>
2020-10-06 11:25:08 +0300

    1

--- src/V1C8/Metadata.cs
+++ src/V1C8/Metadata.cs
@@ -11,11 +11,15 @@ namespace sabatex.V1C8
         internal MetaData(V1C8COMConnector v1C8COMConnector)
         {
             owner = v1C8COMConnector;
+            this.Enums = new Enums(v1C8COMConnector);
+
         }
 
         public string Name { get=>owner.COMObject.Metadata.Name; }
         public string Version { get => owner.COMObject.Metadata.Version; }
         public dynamic FindByType(dynamic elementType)=>owner.COMObject.Metadata.FindByType(elementType);
+
+        public readonly Enums Enums;
 
         public void Dispose()
         {

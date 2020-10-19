f85f298d2270f16428c074404468a50ac051c11f
sabatex <serhiy.lakas@hotmail.com>
2020-10-19 16:24:33 +0300

    fix error

--- src/V1C8/Metadata/CatalogAttributesMetadata.cs
+++ src/V1C8/Metadata/CatalogAttributesMetadata.cs
@@ -2,14 +2,13 @@ using sabatex.V1C8.Models;
 
 namespace sabatex.V1C8.Metadata
 {
-    public class CatalogAttributesMetadata:V1C8COMObject
+    public class CatalogAttributesMetadata:EnumerationsMetadata
     {
         public CatalogAttributesMetadata(V1C8COMObject owner, object handle):base(owner,handle)
         {
             
         }
-        public int Count { get => MethodInt("Count"); }
-        public FieldMetadata Get(int i)=>Method<FieldMetadata>("Get");
+        public FieldMetadata Get(int i)=>base.Get<FieldMetadata>(i);
 
     }
 }

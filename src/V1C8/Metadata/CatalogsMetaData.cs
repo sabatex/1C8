9adec1b20cd83208c95c8be4a10840f98b44efff
sabatex <serhiy.lakas@hotmail.com>
2020-10-19 16:04:16 +0300

    fix type error

--- src/V1C8/Metadata/CatalogsMetaData.cs
+++ src/V1C8/Metadata/CatalogsMetaData.cs
@@ -4,14 +4,13 @@ using System.Text;
 
 namespace sabatex.V1C8.Metadata
 {
-    public class CatalogsMetaData:MetaDataBase
+    public class CatalogsMetaData:EnumerationsMetadata
     {
         public CatalogsMetaData(MetaData owner, object handle):base(owner,handle)
         {
             
         }
-        public int Count { get => MethodInt("Count"); }
-        public CatalogMetaData Get(int i)=>Method<CatalogMetaData>("Get",i);
+        public CatalogMetaData Get(int i)=>base.Get<CatalogMetaData>(i);
  
     }
 }

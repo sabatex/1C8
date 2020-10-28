4300b5e107ad8ce207efcffa390c5797ade68d08
sabatex <serhiy.lakas@hotmail.com>
2020-10-28 16:24:14 +0200

    bug fixed

--- src/V1C8/Metadata/IMetaDataObjectField.cs
+++ src/V1C8/Metadata/IMetaDataObjectField.cs
@@ -2,7 +2,7 @@ using sabatex.V1C8.Models;
 
 namespace sabatex.V1C8.Metadata
 {
-    public interface IMetaDataObjectField:IV1C8COMObject
+    public interface IMetaDataObjectField:IMetaDataDescription
     {
          public IMetaDataTypeDescription Type {get=>GetProperty<IMetaDataTypeDescription>("Type");}
  

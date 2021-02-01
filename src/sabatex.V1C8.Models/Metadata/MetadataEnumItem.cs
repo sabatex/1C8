c54c8ee4d129bb96918a08cce01d1d63a1be6cb7
sabatex <serhiy.lakas@hotmail.com>
2021-02-01 21:55:27 +0200

    add MetadataEnum models

---
+++ src/sabatex.V1C8.Models/Metadata/MetadataEnumItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sabatex.V1C8.Models.Metadata
+{
+    public class MetadataEnumItem:MetadataObject
+    {
+        public int Id { get; set; }
+    }
+}

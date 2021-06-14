14d16b616edaee395e63bd61ecc1c7a41b91541d
sabatex <serhiy.lakas@hotmail.com>
2021-06-14 12:30:35 +0300

    add catalog object functionaliry

--- src/sabatex.V1C8/ApplicationObjects/ICatalogObject.cs
+++ src/sabatex.V1C8/ApplicationObjects/ICatalogObject.cs
@@ -9,6 +9,17 @@ namespace sabatex.V1C8.ApplicationObjects
     public interface ICatalogObject:ICatalogRef
     {
         public void Write() => Method("Write");
-        
+        public new string Code { get => GetProperty<string>("Code"); set => SetProperty("Code", value); }
+        public new string Description { get => GetProperty<string>("Description"); set => SetProperty("Description", value); }
+        public new ICatalogRef Owner { get => GetProperty<ICatalogRef>("Owner"); set => SetProperty("Owner", value); }
+        public new ICatalogRef Parent
+        {
+            get => GetProperty<ICatalogRef>("Parent"); set => SetProperty("Parent", value);
+        }
+        public new bool DeletionMark
+        {
+            get => GetProperty<bool>("DeletionMark"); set => SetProperty("DeletionMark", value);
+        } 
+
     }
 }

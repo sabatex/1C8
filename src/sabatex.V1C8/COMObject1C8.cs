f7654bf947dcac56f0f84ccb4c4c1c84fef1acf9
sabatex <serhiy.lakas@hotmail.com>
2021-06-14 12:17:20 +0300

    fix error

--- src/sabatex.V1C8/COMObject1C8.cs
+++ src/sabatex.V1C8/COMObject1C8.cs
@@ -20,7 +20,7 @@ namespace sabatex.V1C8
                                 // Enum
                                 IEnumsManager,IEnumManager,IEnumRef,
                                 // Catalog
-                                ICatalogRef,ICatalogManager,ICatalogSelection,ICatalogsManager
+                                ICatalogRef,ICatalogObject,ICatalogManager,ICatalogSelection,ICatalogsManager
     {
 
         //private const string _COMServerName = "V83.COMConnector";

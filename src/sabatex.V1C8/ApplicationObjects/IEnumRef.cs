22b05892185fe1528f49f9c168720e0ca56cac12
sabatex <serhiy.lakas@hotmail.com>
2021-06-14 13:29:36 +0300

    extension IEnumRef ValueName

--- src/sabatex.V1C8/ApplicationObjects/IEnumRef.cs
+++ src/sabatex.V1C8/ApplicationObjects/IEnumRef.cs
@@ -5,9 +5,19 @@ using System.Text;
 
 namespace sabatex.V1C8.ApplicationObjects
 {
-    public interface IEnumRef:ICOMObject1C8
+    public interface IEnumRef : ICOMObject1C8
     {
         public IMetadataObjectEnumValue Metadata() => Method<IMetadataObjectEnumValue>("Metadata");
         public bool IsEmpty => Method<bool>("IsEmpty");
+        public string ValueName
+        {
+            get 
+            {
+                var name = Metadata().Name;
+                var index = GlobalContext.Enums[name].IndexOf(this);
+                return GlobalContext.MetaData.Enums[name].EnumValues[index].Name;
+            }
+        }
+ 
     }
 }
